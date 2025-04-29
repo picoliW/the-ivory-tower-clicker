db1931d92f73b2527358524e97ee08f672be63f1
Luiz <leduardo3011@gmail.com>
2025-04-29 00:40:59 -0300

    style: move start bosasfight to a new archive instead main

--- core/main.py
+++ core/main.py
@@ -5,53 +5,28 @@ from shop import Shop
 from ui import UI
 from background import Background
 from screeninfo import get_monitors
-from bossfight import BossFight
-from ursina import Audio
 from pause_menu import PauseMenu
+from start_bossfight import Start_bossfight
 
 app = Ursina()
 
 monitor = get_monitors()[0]
-monitor_width = monitor.width
-monitor_height = monitor.height
-
 window.title = 'The Ivory Tower'
 window.borderless = True
 window.fullscreen = True
-window.size = (monitor_width, monitor_height)
+window.size = (monitor.width, monitor.height)
 
-boss_fight = None
-bossfight_win_sound = Audio('../assets/sounds/bossfightSounds/bossfightWin.wav', autoplay=False)
+player = Player()
+enemy_manager = EnemyManager(player)
+shop = Shop(player)
+ui = UI(player, enemy_manager)
+background = Background()
+pause_menu = PauseMenu()
+start_bossfight = Start_bossfight(player, enemy_manager)
 
 def update():
-    global boss_fight
-
-    if boss_fight and boss_fight.active:
-        boss_fight.update()
-        return
-
-    player.update()
-    enemy_manager.update()
+    start_bossfight.update()
     ui.update()
-
-    if player.floor == 2 and not boss_fight:
-        def win():
-            player.floor += 1
-            player.sprite.position = (-5, 0.6)
-            bossfight_win_sound.play()
-            enemy_manager.show_enemies()
-            global boss_fight
-            boss_fight = None 
-
-        def fail():
-            player.floor -= 1
-            player.sprite.position = (-5, 0.6)
-            enemy_manager.show_enemies()
-            global boss_fight
-            boss_fight = None 
-        enemy_manager.hide_enemies()
-        boss_fight = BossFight(player, on_win=win, on_fail=fail)
-
 
 def input(key):
     if key == 'left mouse down' and not pause_menu.enabled:
@@ -64,14 +39,5 @@ def input(key):
             pause_menu.close()
         else:
             pause_menu.open()
-
-
-player = Player()
-enemy_manager = EnemyManager(player)
-shop = Shop(player)
-ui = UI(player, enemy_manager)
-background = Background()
-pause_menu = PauseMenu()
-
 
 app.run()
---
+++ core/start_bossfight.py
@@ -0,0 +1,37 @@
+from bossfight import BossFight
+from ursina import Audio
+
+class Start_bossfight:
+    def __init__(self, player, enemy_manager):
+        self.player = player
+        self.enemy_manager = enemy_manager
+        self.boss_fight = None
+        self.bossfight_win_sound = Audio('../assets/sounds/bossfightSounds/bossfightWin.wav', autoplay=False)
+
+    def update(self):
+        if self.boss_fight and self.boss_fight.active:
+            self.boss_fight.update()
+            return
+
+        self.player.update()
+        self.enemy_manager.update()
+
+        if self.player.floor == 2 and not self.boss_fight:
+            self.start_bossfight()
+
+    def start_bossfight(self):
+        def win():
+            self.player.floor += 1
+            self.player.sprite.position = (-5, 0.6)
+            self.bossfight_win_sound.play()
+            self.enemy_manager.show_enemies()
+            self.boss_fight = None
+
+        def fail():
+            self.player.floor -= 1
+            self.player.sprite.position = (-5, 0.6)
+            self.enemy_manager.show_enemies()
+            self.boss_fight = None
+
+        self.enemy_manager.hide_enemies()
+        self.boss_fight = BossFight(self.player, on_win=win, on_fail=fail)
