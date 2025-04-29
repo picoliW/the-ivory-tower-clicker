412076df7cc0ed47209e1e61696725d112fe2b65
Luiz <leduardo3011@gmail.com>
2025-04-29 00:33:41 -0300

    fix: fix image path

--- core/shop.py
+++ core/shop.py
@@ -2,21 +2,23 @@ import json
 import os
 
 class ShopItem:
-    def __init__(self, name, texture, cost, stat_type, stat_value, description=""):
+    def __init__(self, name, texture, cost, stat_type, stat_value, image_path, description=""):
         self.name = name
         self.texture = texture
         self.cost = cost
         self.stat_type = stat_type
         self.stat_value = stat_value
+        self.image_path = image_path
         self.description = description
         self.purchased = False
 
 class ArmorItem:
-    def __init__(self, name, texture, base_damage, base_cost):
+    def __init__(self, name, texture, base_damage, base_cost, image_path):
         self.name = name
         self.texture = texture
         self.base_damage = base_damage
         self.base_cost = base_cost
+        self.image_path = image_path
         self.level = 1
         self.max_level = 100
         self.total_damage_added = 0
