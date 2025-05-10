8846f9146a62ba0da9448f0164a73c3763ba3596
Luiz <leduardo3011@gmail.com>
2025-05-09 23:02:42 -0300

    fix: fix register input text size

--- core/auth_forms.py
+++ core/auth_forms.py
@@ -1,7 +1,5 @@
 from ursina import *
 
-
-
 class LoginForm(Entity):
     def __init__(self, parent=None):
         super().__init__(parent=parent)
@@ -14,7 +12,7 @@ class LoginForm(Entity):
             z=-1.1
         )
 
-        self.email_input.text_field.scale = (2, 16)
+        self.email_input.text_field.scale = (2.62, 16)
 
         self.password_input = InputField(
             placeholder='Password',
@@ -23,7 +21,7 @@ class LoginForm(Entity):
             parent=self,
             z=-1.1
         )
-        self.password_input.text_field.scale = (6, 16)
+        self.password_input.text_field.scale = (2.62, 16)
 
 class RegisterForm(Entity):
     def __init__(self, parent=None):
@@ -31,26 +29,29 @@ class RegisterForm(Entity):
 
         self.email_input = InputField(
             placeholder='E-mail',
-            scale=(1.5, 0.4),
-            position=(1.8, 0.4),
+            scale=(4, 0.4),
+            position=(0, -0.5),
             parent=self,
             z=-1.1
         )
+        self.email_input.text_field.scale = (2.62, 16)
 
         self.password_input = InputField(
             placeholder='Senha',
-            scale=(1.5, 0.4),
-            position=(1.8, 0),
+            scale=(4, 0.4),
+            position=(0, -1.2),
             parent=self,
             z=-1.1
         )
+        self.password_input.text_field.scale = (2.62, 16)
 
         self.confirm_password_input = InputField(
             placeholder='Confirmar Senha',
-            scale=(1.5, 0.4),
-            position=(1.8, -0.4),
+            scale=(4, 0.4),
+            position=(0, -1.9),
             parent=self,
             z=-1.1
         )
+        self.confirm_password_input.text_field.scale = (2.62, 16)
 
   
